19927be428ada0b5fffa69a91ad0c6d4b5f3a7b2
Iuri Matias <iuri.matias@gmail.com>
2026-03-27 09:28:35 -0400

    use some c++ types instead of Qt

--- src/inline/call_executor.h
+++ src/inline/call_executor.h
@@ -1,23 +1,23 @@
 #ifndef CALL_EXECUTOR_H
 #define CALL_EXECUTOR_H
 
+#include <string>
+#include <vector>
 #include <QString>
-#include <QStringList>
-#include <QList>
 #include <QJsonValue>
 #include "command_line_parser.h"
 
 class CallExecutor {
 public:
-    static int executeCalls(const QList<ModuleCall>& calls);
+    static int executeCalls(const std::vector<ModuleCall>& calls);
 
 private:
     static QString resolveParam(const QString& param);
-    
+
     static QJsonValue convertParam(const QString& param);
-    
-    static QString buildParamsJson(const QStringList& params);
-    
+
+    static QString buildParamsJson(const std::vector<std::string>& params);
+
     static bool executeCall(const ModuleCall& call);
 };
 
